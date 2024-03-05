729f4fe1ea9ce2ccb53cc910ca3477f51e2f6397
aleixalynn <143188376+aleixalynn@users.noreply.github.com>
2024-03-05 10:21:48 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-var randomizedNum = Math.floor(Math.random()*1000);
+var randomizedNum = Math.floor(Math.random()*1001);
 
 var hundredthsplace = Math.floor(randomizedNum / 100);
 var tenthsplace = Math.floor((randomizedNum % 100)/10);
 var onesplace =  randomizedNum % 10;
 
-var date = new Date(2024, 3, 4, 8, 53, 20);
+var date = new Date();
 
 document.getElementById("randomizednum").innerHTML = "Randomly generated number: " + randomizedNum + "<br>Breakdown: " + hundredthsplace * 100 + ", " + tenthsplace * 10 + ", " + onesplace + "<br>Current Date of Printing: " + date;
 
